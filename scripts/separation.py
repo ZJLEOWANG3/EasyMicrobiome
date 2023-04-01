5390c9c46667e29afd965961e660b3717cd1d3a1
zjleowang <zw634@cornell.edu>
2023-03-31 22:45:02 -0400

    update the pipeline

--- scripts/separation.py
+++ scripts/separation.py
@@ -1,4 +1,14 @@
 ### 
 # This is the code within the EasyMicrobiome package for the separation visualization for either 16S data or more
 ###
-
+from sklearn.decomposition import PCA
+import pandas as pd
+def EMcluster(df:pd.DataFrame,methodology:str):
+    """
+    df :: dataframe n x d
+    method :: str, PCA/NMDS ( nonmetric multi-dimensional/TSNE/UMAP
+scaling)
+    
+    """
+    n, d  = df.shape
+    pca = PCA(n_components=2)
